895d4a59e8ac38057de9cd6af2a4c9cfd58ac23b
AleksanderLib <102597150+AleksanderLib@users.noreply.github.com>
2023-10-23 19:00:23 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace SquareEqualtion_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            double a, b, c;
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Введите a = ");
+                    a = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Введите b = ");
+                    b = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Введите c = ");
+                    c = Convert.ToDouble(Console.ReadLine());
+                    break;
+
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Неверный формат данных");
+                }
+            }
+
+            SquareEqualtion equaltion = new SquareEqualtion();
+            equaltion.GetRoots(a, b, c);
+
+            Output(a, b, c, equaltion);
+
+            Console.ReadLine();
+        }
+
+        private static void Output(double a, double b, double c, SquareEqualtion equaltion)
+        {
+            double[] rezult = equaltion.GetRoots(a, b, c);
+
+            switch (rezult.Length)
+            {
+                case 1:
+                    Console.WriteLine("Уравнение имеет один корень");
+                    Console.WriteLine("x1 = {0}", rezult[0]);
+                    break;
+                case 2:
+                    Console.WriteLine("Уравнение имеет два корня");
+                    Console.WriteLine("x1 = {0} x2 = {1}", rezult[0], rezult[1]);
+                    break;
+                default:
+                    Console.WriteLine("Уравнение не имеет вещественных корней");
+                    break;
+            }
+        }
+    }
+}
---
+++ SquareEqualtion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SquareEqualtion_1
+{
+    public class SquareEqualtion
+    {       
+        public double[] GetRoots(double a, double b, double c)
+        {
+            double Discriminant = Calc_Discriminant(a,b,c); 
+            double x1, x2;
+            
+            if (Discriminant > 0)
+            {
+                x1 = (-1 * b + Math.Sqrt(Discriminant)) / (2 * a);
+                x2 = (-1 * b - Math.Sqrt(Discriminant)) / (2 * a);
+                double[] Roots = new double[] {x1,x2};
+                return Roots;
+
+            }
+            else if (Discriminant == 0)
+            {
+
+                x1 = (-1 * b) / (2 * a);
+                double[] Roots = new double[] {x1};
+                return Roots;
+            }
+            else
+            {
+                double[] Roots = new double[] {};
+                return Roots;
+            }
+        }
+
+        private double Calc_Discriminant(double a,double b, double c)
+        {
+            return Math.Pow(b, 2) - 4 * a * c;
+        }
+    }
+}
